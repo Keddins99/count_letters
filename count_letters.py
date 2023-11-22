94d85b45630351de1dbbffa4af8483d934d42a62
Keddins <eddinskeathan@gmail.com>
2023-11-22 10:50:53 -0800

    count_letters

--- count_letters.py
+++ count_letters.py
@@ -1,14 +1,9 @@
 def count_letters(input_string):
-    # Initialize an empty dictionary to store the letter counts
     letter_count = {}
 
-    # Iterate through each character in the input string
     for char in input_string:
-        # Check if the character is a letter (alphabetical)
         if char.isalpha():
-            # Convert the character to uppercase to ensure case insensitivity
             char = char.upper()
-            # Increment the count for the letter in the dictionary
             letter_count[char] = letter_count.get(char, 0) + 1
 
     return letter_count
