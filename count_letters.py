25fa8989e24d61e2457c16f3054870d6345477c6
Keddins <eddinskeathan@gmail.com>
2023-11-22 10:52:29 -0800

    count_letters

--- count_letters.py
+++ count_letters.py
@@ -2,12 +2,13 @@ def count_letters(input_string):
     letter_count = {}
 
     for char in input_string:
-        if char.isalpha():
-            char = char.upper()
-            letter_count[char] = letter_count.get(char, 0) + 1
+        if char.isalpha():  # Check if the character is a letter
+            char_upper = char.upper()
+            letter_count[char_upper] = letter_count.get(char_upper, 0) + 1
 
     return letter_count
 
+# Example usage:
 input_string = "AaBb"
 result = count_letters(input_string)
 print(result)
